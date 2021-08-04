e85964c51df5c7fe45beb874d82e34f0f15c9ad1
namsing1 <68376424+namsing1@users.noreply.github.com>
2021-08-04 20:32:41 +0200

    Update SolaceListener.java

--- src/main/java/com/ikea/eventmeshtopubsub/SolaceListener.java
+++ src/main/java/com/ikea/eventmeshtopubsub/SolaceListener.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
-
 import javax.jms.BytesMessage;
 import javax.jms.JMSException;
 import javax.jms.Message;
